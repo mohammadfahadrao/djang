722d781613e902f8a362f4443a5caa66b7553b97
Mohammad Fahad <mohammadfahad.se@gmail.com>
2024-03-15 00:08:40 +0500

    Set path for images and root path for store

--- djangopractice/urls.py
+++ djangopractice/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
+from store import views
 
 urlpatterns = [
+    path('',include("store.urls")),
     path('admin/', admin.site.urls),
-    path('books/',include("books.urls"))
-]
+    path('books/',include("books.urls")),
+   
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
